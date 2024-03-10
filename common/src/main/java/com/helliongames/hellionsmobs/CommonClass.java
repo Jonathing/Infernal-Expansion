ddf4989c16f0714f578fe17c08f5157e447a87e0
Darien Gillespie <dsgillespie02@yahoo.com>
2024-03-10 11:12:57 -0400

    Set up registration system

--- common/src/main/java/com/helliongames/hellionsmobs/CommonClass.java
+++ common/src/main/java/com/helliongames/hellionsmobs/CommonClass.java
@@ -1,11 +1,13 @@
 package com.helliongames.hellionsmobs;
 
 import com.helliongames.hellionsmobs.platform.Services;
+import com.helliongames.hellionsmobs.registration.HellionsMobsCreativeTabs;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
 public class CommonClass {
 
     public static void init() {
+        HellionsMobsCreativeTabs.loadClass();
     }
 }
