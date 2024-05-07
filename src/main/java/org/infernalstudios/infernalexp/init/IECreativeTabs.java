082106b42c6d092a210b5a13ead16fdd8e64b68d
Lyof <florian.lyof@laposte.net>
2024-05-07 13:36:02 +0200

    added items to ie tab (in order of registry for now)

--- src/main/java/org/infernalstudios/infernalexp/init/IECreativeTabs.java
+++ src/main/java/org/infernalstudios/infernalexp/init/IECreativeTabs.java
@@ -15,7 +15,45 @@ public class IECreativeTabs {
     public static final Supplier<CreativeModeTab> TAB = CREATIVE_TABS.register("InfernalTab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(IEItems.TAB_ITEM.get())).displayItems(IECreativeTabs::displayItems).build());
 
     private static void displayItems(CreativeModeTab.ItemDisplayParameters itemDisplayParameters, CreativeModeTab.Output output) {
-        // TODO
+        output.accept(IEItems.GLOWCOAL.get());
+        output.accept(IEItems.DULLROCKS.get());
+        output.accept(IEItems.MOTH_DUST.get());
+        output.accept(IEItems.MOLTEN_GOLD_CLUSTER.get());
+        output.accept(IEItems.SOUL_SALT_CLUMP.get());
+
+        output.accept(IEItems.BLINDSIGHT_TONGUE.get());
+        output.accept(IEItems.BLINDSIGHT_TONGUE_STEW.get());
+        output.accept(IEItems.CURED_JERKY.get());
+        output.accept(IEItems.SPIRIT_EYE.get());
+
+        output.accept(IEItems.RAW_HOGCHOP.get());
+        output.accept(IEItems.COOKED_HOGCHOP.get());
+
+        output.accept(IEItems.VOLINE_SPAWN_EGG.get());
+        output.accept(IEItems.SHROOMLOIN_SPAWN_EGG.get());
+        output.accept(IEItems.WARPBEETLE_SPAWN_EGG.get());
+        output.accept(IEItems.EMBODY_SPAWN_EGG.get());
+        output.accept(IEItems.BASALT_GIANT_SPAWN_EGG.get());
+        output.accept(IEItems.GLOWSQUITO_SPAWN_EGG.get());
+        output.accept(IEItems.BLACKSTONE_DWARF_SPAWN_EGG.get());
+        output.accept(IEItems.BLINDSIGHT_SPAWN_EGG.get());
+        output.accept(IEItems.GLOWSILK_MOTH_SPAWN_EGG.get());
+
+        output.accept(IEItems.VOLINE_BUCKET.get());
+        output.accept(IEItems.MAGMA_CUBE_BUCKET.get());
+        output.accept(IEItems.STRIDER_BUCKET.get());
+
+        output.accept(IEItems.GLOW_TORCH.get());
+        output.accept(IEItems.DULLTHORNS.get());
+        output.accept(IEItems.ASCUS_BOMB.get());
+        output.accept(IEItems.INFERNAL_PAINTING.get());
+
+        output.accept(IEItems.MUSIC_DISC_SOUL_SPUNK.get());
+        output.accept(IEItems.MUSIC_DISC_FLUSH.get());
+
+        output.accept(IEItems.GLOWSILK_BOW.get());
+        output.accept(IEItems.BLINDSIGHT_TONGUE_WHIP.get());
+        output.accept(IEItems.KINETIC_TONGUE_WHIP.get());
     }
 
     public static void register(IEventBus modEventBus) {
